bab2629b0dd0ee1d4dece84a908babf3479605e1
Danishaft <danielejeh2019@gmail.com>
2024-08-06 00:04:54 +0100

    initialize folder routes and methods

---
+++ server/routes/folders.js
@@ -0,0 +1,12 @@
+const { Router } = require('express')
+const {getFolder, addFolder, getAllFolders} = require("../controllers/folders")
+const authenticate = require('../middleware/auth')
+
+const router = Router()
+router.use(authenticate)
+
+router.post('/', addFolder)
+router.get('/:folder_id', getFolder) 
+router.get('/', getAllFolders)
+
+module.exports = router
