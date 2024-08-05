0844247748194ef6655192da97d8a89e1085ddea
Danishaft <danielejeh2019@gmail.com>
2024-08-06 00:13:05 +0100

    add db helper functions for users

---
+++ server/helpers/users.js
@@ -0,0 +1,55 @@
+const { PrismaClient } = require('@prisma/client')
+const prisma = new PrismaClient()
+
+
+const createUser = async (name, email, member_id) => {
+    try {
+        const user = await prisma.user.create({
+            data: {
+                id: member_id,
+                name,
+                email
+            }
+        })
+        return user;
+    }catch(error){
+        throw new Error('Could not create user', error.message);
+    }finally{
+        await prisma.$disconnect()
+    }
+}
+
+const getUser = async (member_id) => {
+    try {
+        const user = await prisma.user.findUnique({
+            where: {
+                id: member_id
+            }
+        })
+        return user;
+    }catch(error){
+        throw new Error('Could not get user', error.message);
+    }finally{
+        await prisma.$disconnect()
+    }
+}
+
+const updateUser = async (member_id, name) => {
+    try {
+        const user = await prisma.user.update({
+            where: {id: member_id},
+            data: {name: name}
+        })
+        return user
+    }catch(error){
+        throw new Error('Could not update user', error.message);
+    }finally{
+        await prisma.$disconnect()
+    }
+}
+
+module.exports = {
+    createUser,
+    getUser,
+    updateUser
+}
