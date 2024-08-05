4e264dd86c9ce2ccb6a5a12c6130de1f493d5178
Danishaft <danielejeh2019@gmail.com>
2024-08-06 00:27:11 +0100

    add navbr and sidebar and main in a layout

---
+++ client/src/components/Layout.tsx
@@ -0,0 +1,17 @@
+import { Outlet } from "react-router-dom"
+import {SideNav} from "./SideNav"
+import { Navbar } from "./Navbar"
+
+export const Layout = () => {
+  return (
+    <div className="flex h-screen">
+      <SideNav />
+      <div className="flex-1 flex flex-col ml-64 overflow-hidden">
+        <Navbar />
+        <main className="flex-1 overflow-x-hidden overflow-y-auto bg-[#fdfdfd] p-4">
+          <Outlet />
+        </main>
+      </div>
+    </div>
+  )
+}
