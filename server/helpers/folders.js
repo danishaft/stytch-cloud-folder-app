25a048d09f81f6139c4f271cdffc9e02ff15233b
Danishaft <danielejeh2019@gmail.com>
2024-08-06 00:14:05 +0100

    add db helper functions for folders

---
+++ server/helpers/folders.js
@@ -0,0 +1,53 @@
+const { PrismaClient } = require('@prisma/client')
+const prisma = new PrismaClient()
+
+const createFolder = async (name, member_id) => {
+    try{
+        const folder = await prisma.folder.create({
+            data: {
+                name,
+                creator: {connect: {id: member_id}},
+            }
+        })
+        return folder;
+    }catch(error){
+        console.error(error.message)
+        throw new Error('Could not create folder', error.message);
+    }finally{
+        await prisma.$disconnect()
+    }
+}
+
+const fetchFolder = async (folder_id) => {
+    try{
+        const folder = await prisma.folder.findUnique({
+            where: {id: folder_id}
+        })
+        return folder;
+    }catch(error){
+        console.error(error.message)
+        throw new Error('Could not get folder', error.message);
+    }finally{
+        await prisma.$disconnect()
+    }
+}
+
+const fetchFolders = async (member_id) => {
+    try{
+        const folders = await prisma.folder.findMany({
+            where: {creatorId: member_id}
+        })
+        return folders;
+    }catch(error){
+        console.error(error.message)
+        throw new Error('Could not fetch folder', error.message);
+    }finally{
+        await prisma.$disconnect()
+    }
+}
+
+module.exports = {
+    createFolder,
+    fetchFolder,
+    fetchFolders
+}
