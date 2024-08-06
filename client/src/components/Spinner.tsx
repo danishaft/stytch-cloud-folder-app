3421e83da6c1f14169f05c8bc8894f557fbe0088
Danishaft <danielejeh2019@gmail.com>
2024-08-06 11:59:36 +0100

    delete 7 files

--- client/src/components/Spinner.tsx
+++
@@ -1,8 +0,0 @@
-
-export const Spinner = () => {
-    return (
-      <div className="flex justify-center items-center h-screen">
-        <div className="w-16 h-16 border-4 border-[#0061FF] border-t-transparent border-solid rounded-full animate-spin"></div>
-      </div>
-    )
-  }
